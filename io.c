df4f2c7e0055b92fa42b765ff8e5597af4b8344f
Bryan Hinton <computerprogrammer1976@gmail.com>
2022-07-01 07:45:01 -0500

    Initial import

---
+++ io.c
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * io.c
+ *
+ * Copyright (C) 2022 Bryan Hinton
+ *
+ */
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+    return EXIT_SUCCESS;
+}
