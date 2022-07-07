5db0f5db03fd66014145831b16a1e4839bec6244
Bryan Hinton <computerprogrammer1976@gmail.com>
2022-07-07 14:19:34 -0500

    Print 64-bit pointer address

--- io.c
+++ io.c
@@ -5,6 +5,7 @@
  * Copyright (C) 2022 Bryan Hinton
  *
  */
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,6 +13,9 @@ int main(int argc, char **argv)
 {
 	if(argc > 1) 
 		return EXIT_FAILURE;
+
+	void *fptr = &main;
+	printf("fptr 0x%.12"PRIXPTR"\n", (uintptr_t)fptr);
 
 	return EXIT_SUCCESS;
 }
